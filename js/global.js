2cb626572dac346253c989272bb2742f5eb1c94c
Paolo Zilioti <pzilioti@gmail.com>
2021-07-27 14:09:23 -0300

    persistent language selection

--- js/global.js
+++ js/global.js
@@ -11,16 +11,16 @@ jQuery(function ($) {
         'pt': 'i18n/pt.json',
         'cn': 'i18n/cn.json'
     }).done(function () {
+        var locale = sessionStorage.getItem('locale');
+        if (locale) {
+            $.i18n().locale = locale;
+        }
         $('html').i18n();
-
-//        History.Adapter.bind(window, 'statechange', function () {
-//            set_locale_to(url('?locale'));
-//        });
 
         $('.switch-locale').on('click', 'a', function (e) {
             e.preventDefault();
-            //History.pushState(null, null, "?locale=" + $(this).data('locale'));
             set_locale_to($(this).data('locale'));
+            sessionStorage.setItem('locale', $(this).data('locale'));
         });
 
     });
